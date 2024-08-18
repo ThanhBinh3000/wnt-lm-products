00868b5c11f84894e41004f483a05e908c9ee47d
vuductinh97 <vuductinh97@gmail.com>
2024-08-18 10:21:18 +0700

    Update HangHoaES.java

--- src/main/java/vn/com/gsoft/product/model/dto/elastichsearch/HangHoaES.java
+++ src/main/java/vn/com/gsoft/product/model/dto/elastichsearch/HangHoaES.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
 @Builder
@@ -15,5 +17,6 @@ import org.springframework.data.elasticsearch.annotations.Document;
 public class HangHoaES {
     @Id
     private String id;
+    @Field(type = FieldType.Text)
     private String name;
 }
